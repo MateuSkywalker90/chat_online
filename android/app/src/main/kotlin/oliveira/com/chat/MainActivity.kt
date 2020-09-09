1434be1323a86af9b973c9158ee651001142fe9a
Mateus Oliveira <mateus.oliveira@gruposaopedro.local>
2020-09-09 12:36:20 -0400

    Source

---
+++ android/app/src/main/kotlin/oliveira/com/chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package oliveira.com.chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:chat/chat_screen.dart';
+
+void main() async {
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Demo",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue,
+        ),
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
